31ebc8b566683bd8d6b8d33d539db238ea96b4cf
Hyun Mi Maust <hyunmimaust@gmail.com>
2017-07-03 09:10:54 -0700

    Add empty config file.

---
+++ src/main/java/com/example/android/popularmovies/Config.java
@@ -0,0 +1,11 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by hmaust on 6/30/17.
+ */
+
+public class Config {
+
+    // TODO Insert API Key Below
+    public static final String apiKey = "";
+}
